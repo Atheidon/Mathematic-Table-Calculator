be5f97df2513bfe11cac014c5d14451b79a4cac2
Atheidon <93974381+Atheidon@users.noreply.github.com>
2021-11-15 17:14:13 +0100

    Update Mathematic Tables Calculator compact.py

--- Mathematic Tables Calculator compact.py
+++ Mathematic Tables Calculator compact.py
@@ -1,4 +1,4 @@
-number = int(input("What do you want to calculate the tables of: "))
-amount = int(input("What is the extend: "))
-for i in range(1,amount + 1): print(i,"*",number,"=",i*number)
+n = int(input("What do you want to calculate the tables of: "))
+a = int(input("What is the extend: "))
+for i in range(1,a + 1): print(i,"*",n,"=",i*n)
 input("press enter to exit")
